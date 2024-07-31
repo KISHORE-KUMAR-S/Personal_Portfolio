59df0c3a97298654ccfeeef30d0345081ef6d99b
KISHORE-KUMAR-S <livekishore2001@gmail.com>
2024-08-01 00:01:03 +0530

    Sixth Commit

--- lib/config/colors_config.dart
+++ lib/config/colors_config.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart' show Colors;
+import 'package:flutter/material.dart' show Color, Colors;
 
 class ColorsConfig {
   static const primary = Colors.black;
-  static const secondary = Colors.lightBlueAccent;
+  static const secondary = Color.fromARGB(255, 58, 90, 152);
   static const red = Colors.red;
   static const white = Colors.white;
 }
